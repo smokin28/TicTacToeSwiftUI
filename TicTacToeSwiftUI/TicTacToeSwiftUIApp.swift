9daacfa97906b1d9ea9b9c0c07cab2605a7dd9ec
Przemyslaw Bobkowski <przemek.bobkowski@icloud.com>
2023-01-22 17:29:51 -0500

    Initial Commit

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Przemyslaw Bobkowski on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
